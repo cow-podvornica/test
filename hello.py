42210b2260c9eb8d84c61b78ec2e959b7c3792b4
Nikita Spiridonov <nspiridonov@ozon.ru>
2022-12-21 20:24:28 +0500

    change hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
-print('hello world!')
-print('hello again!')
-print('hello 3!')
+#!/usr/bin/python
+
+import time
+
+i = 1
+while True:
+    print(f'hello world {i}!')
+    time.sleep(1)
+    i += 1
